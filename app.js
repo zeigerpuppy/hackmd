1fcfcd7a7cbb4a2b7919b276c4bfe415dd165a8f
BoHong Li <a60814billy@gmail.com>
2017-04-12 06:01:22 +0800

    fix(app.js): Fixed typo

--- app.js
+++ app.js
@@ -275,7 +275,7 @@ app.post('/uploadimage', function (req, res) {
   })
 })
 
-app.use(require('./lib/web/noteRoute'))
+app.use(require('./lib/web/noteRouter'))
 
 // response not found if no any route matxches
 app.get('*', function (req, res) {
